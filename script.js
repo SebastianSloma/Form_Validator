a0efc843770109012018532482745b01a1cfd0ee
Sebastian <sebastiansloma@gmail.com>
2023-02-24 14:37:36 +0100

    Sending the form and checking for errors

--- script.js
+++ script.js
@@ -48,14 +48,28 @@ const checkPassword = (pass1, pass2) => {
 };
 
 const checkEmail = (email) => {
-	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(re.test(email.value)){
-        clearError(email)
-    }else{
-        showError(email, 'Email is incorrect')
-    }
-		
+	if (re.test(email.value)) {
+		clearError(email);
+	} else {
+		showError(email, 'Email is incorrect');
+	}
+};
+
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.form-box');
+	let errorCount = 0;
+
+	allInputs.forEach((el) => {
+		if (el.classList.contains('error')) {
+			errorCount++;
+		}
+	});
+	if (errorCount === 0) {
+		popup.classList.add('show-popup');
+	}
 };
 
 sendBtn.addEventListener('click', (e) => {
@@ -65,7 +79,8 @@ sendBtn.addEventListener('click', (e) => {
 	checkLength(username, 4);
 	checkLength(pass, 8);
 	checkPassword(pass, pass2);
-    checkEmail(email)
+	checkEmail(email);
+	checkErrors();
 });
 
 clearBtn.addEventListener('click', (e) => {
@@ -73,5 +88,6 @@ clearBtn.addEventListener('click', (e) => {
 
 	[username, pass, pass2, email].forEach((el) => {
 		el.value = '';
+		clearError(el);
 	});
 });
